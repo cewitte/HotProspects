a0f7e6302a9fa336543e4f967682f041bd441274
Carlos Eduardo Witte <cewitte@icloud.com>
2025-06-17 08:40:56 -0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Carlos Eduardo Witte on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
